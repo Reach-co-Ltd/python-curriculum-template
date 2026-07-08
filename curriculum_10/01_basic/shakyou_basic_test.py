22de3f80f87939dbc585b7223de8b7b00ed89771
T-kakeru <145729524+T-kakeru@users.noreply.github.com>
2026-07-09 08:56:37 +0900

    Delete curriculum_10 directory

--- curriculum_10/01_basic/shakyou_basic_test.py
+++
@@ -1,4 +0,0 @@
-# curriculum_10 / 01. 基本概念と導入 / 写経問題
-# バックログの写経内容を1文字ずつここに書いてください
-# 実行: pytest 01_basic/shakyou_basic_test.py -v
-
--- curriculum_10/04_fixture/shakyou_fixture_test.py
+++
@@ -1,4 +0,0 @@
-# curriculum_10 / 04. fixtureを用いた前準備 / 写経問題
-# バックログの写経内容を1文字ずつここに書いてください
-# 実行: pytest 04_fixture/shakyou_fixture_test.py -v
-
